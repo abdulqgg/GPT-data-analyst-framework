4162cfa50dac918ef45c531416abadd63a284760
abdulqgg <abdul.gilani@gmail.com>
2023-08-16 17:04:53 +0100

    updated hosts

--- django/DAframework/DAframework/settings.py
+++ django/DAframework/DAframework/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','abdulqg.pythonanywhere.com']
 
 
 # Application definition
