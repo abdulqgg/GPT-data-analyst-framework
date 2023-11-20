b505d3860cc5b0e82ab057c6c7473f7a2877f2fe
abdulqgg <abdul.gilani@gmail.com>
2023-11-20 20:19:49 +0000

    updated visualsation design

--- django/DAframework/python-execute.py
+++ django/DAframework/python-execute.py
@@ -1,14 +1,20 @@
 import json
 import plotly.express as px
-import pandas as pd
 
-data = {
-    'Song': ['The Trooper', 'Untitled', 'The Number Of The Beast', 'Sure Know Something', 'Hallowed Be Thy Name', 'Eruption', 'Where Eagles Dare', 'Welcome Home (Sanitarium)', 'Sweetest Thing', 'Surrender'],
-    'Rating': [5, 4, 4, 4, 4, 4, 3, 3, 3, 3]
-}
+data = [
+    ('The Trooper', 5),
+    ('Untitled', 4),
+    ('The Number Of The Beast', 4),
+    ('Sure Know Something', 4),
+    ('Hallowed Be Thy Name', 4),
+    ('Eruption', 4),
+    ('Where Eagles Dare', 3),
+    ('Welcome Home (Sanitarium)', 3),
+    ('Sweetest Thing', 3),
+    ('Surrender', 3)
+]
 
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, columns=['Song', 'Rating'])
 
-fig = px.bar(df, x='Song', y='Rating')
-fig.update_layout(title_text='Song Ratings')
+fig = px.bar(df, x='Song', y='Rating', title='Song Ratings')
 print(fig.to_json())
