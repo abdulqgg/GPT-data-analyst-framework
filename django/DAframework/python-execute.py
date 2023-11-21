14a1b782006935c59da78a790b1669fa021407ea
abdulqgg <abdul.gilani@gmail.com>
2023-11-21 16:07:22 +0000

    purple design

--- django/DAframework/python-execute.py
+++ django/DAframework/python-execute.py
@@ -1,19 +1,10 @@
 import json
-import plotly.graph_objects as go
+import plotly.express as px
 
-# Define the data
-songs = ['The Trooper', 'Untitled', 'The Number Of The Beast', 'Sure Know Something',
-         'Hallowed Be Thy Name', 'Eruption', 'Where Eagles Dare', 'Welcome Home (Sanitarium)',
-         'Sweetest Thing', 'Surrender']
-ratings = [5, 4, 4, 4, 4, 4, 3, 3, 3, 3]
+data = {
+    'Song': ['The Trooper', 'Untitled', 'The Number Of The Beast', 'Sure Know Something', 'Hallowed Be Thy Name', 'Eruption', 'Where Eagles Dare', 'Welcome Home (Sanitarium)', 'Sweetest Thing', 'Surrender'],
+    'Rating': [5, 4, 4, 4, 4, 4, 3, 3, 3, 3]
+}
 
-# Create the bar chart
-fig = go.Figure(data=go.Bar(x=songs, y=ratings))
-
-# Set the layout
-fig.update_layout(title='Songs Ratings',
-                  xaxis_title='Songs',
-                  yaxis_title='Ratings')
-
-# Show the chart
+fig = px.bar(data, x='Song', y='Rating', title='Song Ratings')
 print(fig.to_json())
